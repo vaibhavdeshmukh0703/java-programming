2f3fe76bd94662d4343d078705f954f91b7d63b2
Vaibhav Deshmukh <54410273+vaibhavdeshmukh0703@users.noreply.github.com>
2024-04-08 08:37:35 +0000

    Thread programming

--- Client.java
+++ Client.java
@@ -1,6 +1,13 @@
 class Client{
-public static void main(String [] args)
-{
-  System.out.println("Hello world");
-}
+  public static void main(String [] args)
+  {
+    System.out.println("Hello world");
+    System.out.println(Thread.currentThread().getName());
+    doSomething();
+  }
+  public static void doSomething(){
+    System.out.println("From do someting method");
+    System.out.println(Thread.currentThread().getName());
+  }
+
 }
